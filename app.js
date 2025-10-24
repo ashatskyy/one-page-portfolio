6fd87a6affd806d4a0f3cddd8eb96f7c40afcdfc
Alexandr Shatskyy <autome@ukr.net>
2025-10-24 10:13:15 +0200

    scrolling and fonts rectification

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+
+    const isFirefox = typeof InstallTrigger !== 'undefined';
+
+    const scrollAmountMultiplier = 2; 
+
+    if (isFirefox) {
+        document.addEventListener('wheel', (e) => {
+            e.preventDefault();
+
+         
+            const newScrollAmount = e.deltaY * scrollAmountMultiplier;
+
+            window.scrollBy({
+                top: newScrollAmount, 
+                behavior: 'smooth'
+            });
+        }, { passive: false });
+    }
+
+
