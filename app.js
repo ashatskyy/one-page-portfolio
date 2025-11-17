f84c854112d2b5bfb74a11315c9f4dde9f71f6d9
Alexandr Shatskyy <autome@ukr.net>
2025-11-17 10:33:12 +0100

    flicker layout solution

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-'use strict'
+"use strict";
 
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
 
-    const isFirefox = typeof InstallTrigger !== 'undefined';
+const isFirefox = typeof InstallTrigger !== "undefined";
 
-    const scrollAmountMultiplier = 0.7; 
+const scrollAmountMultiplier = 0.7;
 
-    if (isFirefox) {
-        document.addEventListener('wheel', (e) => {
-            e.preventDefault();
+if (isFirefox) {
+  document.addEventListener(
+    "wheel",
+    (e) => {
+      e.preventDefault();
 
-         
-            const newScrollAmount = e.deltaY * scrollAmountMultiplier;
+      const newScrollAmount = e.deltaY * scrollAmountMultiplier;
 
-            window.scrollBy({
-                top: newScrollAmount, 
-                behavior: 'smooth'
-            });
-        }, { passive: false });
-    }
-
-
+      window.scrollBy({
+        top: newScrollAmount,
+        behavior: "smooth",
+      });
+    },
+    { passive: false }
+  );
+}
